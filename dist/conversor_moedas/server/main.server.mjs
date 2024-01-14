96e0f2b11c2e51b6fc430b75e250f25659266903
Valdemir Junior <valdemirvoltatoni@gmail.com>
2024-01-14 09:18:13 -0300

    update

---
+++ dist/conversor_moedas/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-YT2P5WCL.mjs";import"./chunk-XRNI7FC7.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,13 @@ import { RouterOutlet } from '@angular/router';
 import { CardComponent } from "./components/card/card.component";
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { BuscaMoedaService } from './services/busca_usdbrl';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     imports: [CommonModule, RouterOutlet, CardComponent, HttpClientModule, FormsModule],
+    providers:[{provide: LocationStrategy, useClass: HashLocationStrategy}],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
 })
